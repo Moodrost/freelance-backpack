ebc425fbb86b78fab9c72e73cc85cdb7793a10a4
Moodrost <109980141+Moodrost@users.noreply.github.com>
2025-08-05 11:21:46 +0300

    Add Vue router and Pinia setup

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>Shared count: {{ counter.count }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '../stores/counter';
+
+const counter = useCounterStore();
+</script>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '../stores/counter';
+
+const counter = useCounterStore();
+</script>
